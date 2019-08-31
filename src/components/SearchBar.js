df3fc355c0f8cd86c10f6f682c323f66ab986c6f
Anna Mendoza <annamendoza.curve@gmail.com>
2019-08-30 22:53:01 -0700

    built out a SearchBar as a component with three functions to be used, they are not quite dialed up, but will be updating the state in the next section

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,44 @@
 import React from 'react';
 import './searchBar.css'; 
 
+let searchQuery = '';
+
 class SearchBar extends React.Component {
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		search:'',
+  		message: ''
+  	}
+  }
+  
+  onInputChange(event){
+    event.preventDefault();
+    console.log( event.target.value );
+    searchQuery = event.target.value;
+	}
+  onInputSubmit(event, searchQuery){
+  	event.preventDefault();
+  	console.log(searchQuery);
+    this.setState({search: searchQuery})
+  }
+  onInputClick(event){
+  	event.preventDefault();
+  	console.log("Something was clicked")
+  }
+
   render() {
 	  return (
 	    <div className="ui segment">
 	    	<form className="ui form">
 	    		<div className="field">
 	    		  <label className="">Image Search</label>
-	          <input type="text" />
+	          <input type="text" onChange={this.onInputChange} />
+	          <button onSubmit={this.onInputSubmit}>submit</button>
 	        </div>
-	      </form>  
+	      </form>
+	      <div>{this.state.search}</div>  
+	      <div>{this.state.message}</div>  
 	    </div>
 	  );
 	}
