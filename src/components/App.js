25271b0d06a8b4ee2e420ff08312f1b695cdb86b
Anna Mendoza <annamendoza.curve@gmail.com>
2019-09-01 02:02:51 -0700

    made a fetch request with axios wihch returned a set of data in an array

--- src/components/App.js
+++ src/components/App.js
@@ -8,29 +8,36 @@ class App extends React.Component {
   	super(props);
   	this.state = {
   		term:'',
+  		collection: []
   	}
   }
-
-  // Access Key: 
-  // 8dd20cb2d30579d1d53552368b25abae7d7dc7a7141ebdb2a35940833c3e5572
   
-  // Secret Key:
-  // f2e42aaa1157e4e36e4a48122dd38216cb4f2c05e9e31289d31005995988d84e
-  
-  onSearchSubmit(term){
-  	console.log(term);
-  	axios.get('https://api.unsplash.com/search/photos', {
+  async onSearchSubmit(term){
+  	const response = await axios.get('https://api.unsplash.com/search/photos', {
   		params:  { query: term },
       headers: {
       	Authorization: 'Client-ID 8dd20cb2d30579d1d53552368b25abae7d7dc7a7141ebdb2a35940833c3e5572'
       } 
   	});
+
+  	console.log(response.data.results);
+  }
+
+  displayImages = (collection) => {
+  	collection.map((image, index) => {
+  		return `<ul>
+  		         <li>
+  		           <img id=${index} src=${image.urls.regular} />
+  		         </li>
+  		        </ul>` 
+  	})
   }
 
 	render() {
 	  return (
 	    <div className="ui container tinyForm">
 	      <SearchBar onSubmit={this.onSearchSubmit}/>
+	      {this.displayImages(this.state.collection)}
 	    </div>
 	  );
 	}
