241d9131e728f8990587000415687c664e4b490e
Anna Mendoza <annamendoza.curve@gmail.com>
2019-09-05 17:31:45 -0700

    finished creating the ImageList Component using the properties of the object by descrtructuring them when we map them and now doing the same with the displayIMages function I baked up

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ImageList = (props) => {
 	//console.log(props.images);
-  const allImages = props.images.map( (image) => {
-  		return <li key={image.id}><img src={image.urls.regular} alt="UNSPLASH ROCKS" /></li> 
-  	})
+  const allImages = props.images.map( ({description, id, urls}) => {
+  		return <li key={id}><img src={urls.regular} alt={description} /></li> 
+  	});
 	return <>{allImages}</>
 };
 
