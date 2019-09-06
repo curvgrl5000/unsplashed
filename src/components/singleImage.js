05840460d506a976a6b23b1b35ef526f09afeaa9
Anna Mendoza <annamendoza.curve@gmail.com>
2019-09-06 15:46:05 -0700

    started to working on setting up the ref system with a comparison to vanilla javascript, but it could be better explained

--- src/components/singleImage.js
+++ src/components/singleImage.js
@@ -22,3 +22,14 @@ class singleImage extends React.Component{
 }
 
 export default singleImage;
+
+// To get the height of the image in vanilla javascript:
+// var k = document.querySelector('img').clientHeight;
+
+// Instead of doing it the ol' vanilla javascript way
+// we do it the ref way; 
+// React Refs give access to a single DOM element and 
+// allows you to create refs in the constructor, which manages the state
+// and then pass it to a JSX element as a props object
+// which is where we would get the height to calculate the size 
+// of the grid we're building.
