4560cf0b98441780c9a107a88021aa6442a4b0e6
Anna Mendoza <annamendoza.curve@gmail.com>
2019-09-08 14:33:20 -0700

    added more styling for the mobile version

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -3,14 +3,14 @@ import SingleImage from './singleImage';
 import './imageLayout.css';
 
 const ImageList = (props) => {
-	console.log( props.images.length );
+	// console.log( props.images.length );
   if( props.images.length > 0 ) {
   	const allImages = props.images.map( (image) => {
   	  return <SingleImage key={image.id} image={image} />
   	});
 	  return <div className="imageLayout">{allImages}</div>
   } if( props.images.length === 0) {
-  		console.log( `${props.error}` );
+  		// console.log( `${props.error}` );
   		return <h2 className="noResults">{props.error}</h2>
   }
   return null;
